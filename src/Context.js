76bdf481d240c288a01d9d80bf3e206ad2c65e38
Flavio Carvalho <flavio@imflavio.com>
2019-02-07 14:27:25 +0000

    Clean up context file

--- src/Context.js
+++ src/Context.js
@@ -6,40 +6,40 @@ import Prevent from './Prevent'
 export default props => {
   if (!props.reducer) throw new Error('A reducer must be provided.')
   if (!props.actions) throw new Error('A set of actions must be provided.')
-  const { Provider, Consumer } = createContext()
+  const Context = createContext()
 
-  const defaultMapState = () => undefined
-  const defaultMapActions = () => undefined
+  const defaultMap = () => undefined
+
+  const Provider = ({ children }) => (
+    <LocalReducer {...props}>
+      {(state, actions, dispatch) => (
+        <Context.Provider value={{ state, actions, dispatch }}>
+          {children}
+        </Context.Provider>
+      )}
+    </LocalReducer>
+  )
+
+  const Consumer = ({
+    children,
+    mapState = defaultMap,
+    mapActions = defaultMap,
+  }) => (
+    <Context.Consumer>
+      {({ state, actions, dispatch }) => (
+        <Prevent
+          state={mapState(state)}
+          actions={mapActions(actions, dispatch)}
+          dispatch={dispatch}
+        >
+          {children}
+        </Prevent>
+      )}
+    </Context.Consumer>
+  )
 
   return {
-    Provider: ({ children }) => (
-      <LocalReducer {...props}>
-        {(state, actions, dispatch) => (
-          <Provider value={{ state, actions, dispatch }}>{children}</Provider>
-        )}
-      </LocalReducer>
-    ),
-
-    Consumer: ({
-      children,
-      mapState = defaultMapState,
-      mapActions = defaultMapActions,
-    }) => (
-      <Consumer>
-        {({ state, actions, dispatch }) => (
-          <Prevent
-            state={
-              typeof mapState === 'function'
-                ? mapState(state)
-                : defaultMapState(state)
-            }
-            actions={mapActions(actions, dispatch)}
-            dispatch={dispatch}
-          >
-            {children}
-          </Prevent>
-        )}
-      </Consumer>
-    ),
+    Provider,
+    Consumer,
   }
 }
