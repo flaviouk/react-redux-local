5ef14aa6385ecb120617f6f77b4050aa025406a4
Flavio Carvalho <flavio@imflavio.com>
2019-02-07 14:19:44 +0000

    Refactor tests to use react-testing-libary

--- src/__tests__/LocalReducer.test.js
+++ src/__tests__/LocalReducer.test.js
@@ -1,32 +1,59 @@
-import 'raf/polyfill'
 import React from 'react'
-import { mount, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import { createSerializer } from 'enzyme-to-json'
 import { takeEvery } from 'redux-saga'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 
 import LocalReducer from '../..'
 import { reducer, actions, types } from '../duck'
 
-configure({ adapter: new Adapter() })
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
+afterEach(cleanup)
 
 it('should be defined', () => {
   expect(LocalReducer).toBeDefined()
 })
 
-let wrapper = null
-let middlewareCallback = null
-let sagaCallback = null
+const sharedProps = {
+  reducer: reducer,
+  actions: actions,
+  children: ({ counter }, action) => (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={action.increase}>Increase</button>
+      <button onClick={action.decrease}>Decrease</button>
+      <button onClick={action.reset}>Reset</button>
+    </>
+  ),
+}
 
-beforeEach(() => {
-  middlewareCallback = jest.fn()
-  sagaCallback = jest.fn()
+const getCounterText = ({ getByText }) => getByText(/Counter: /i).textContent
+const getIncreaseButton = ({ getByText }) => getByText(/Increase/i)
+const getDecreaseButton = ({ getByText }) => getByText(/Decrease/i)
+const getResetButton = ({ getByText }) => getByText(/Reset/i)
 
-  const middleware = store => next => action => {
-    middlewareCallback()
-    return next(action)
-  }
+test('should render different states based on actions: ', () => {
+  const container = render(<LocalReducer {...sharedProps} />)
+
+  const increaseButton = getIncreaseButton(container)
+  const decreaseButton = getDecreaseButton(container)
+  const resetButton = getResetButton(container)
+
+  expect(getCounterText(container)).toBe('Counter: 0')
+
+  fireEvent.click(increaseButton)
+  expect(getCounterText(container)).toBe('Counter: 1')
+  fireEvent.click(increaseButton)
+  expect(getCounterText(container)).toBe('Counter: 2')
+
+  fireEvent.click(resetButton)
+  expect(getCounterText(container)).toBe('Counter: 0')
+
+  fireEvent.click(decreaseButton)
+  expect(getCounterText(container)).toBe('Counter: -1')
+  fireEvent.click(decreaseButton)
+  expect(getCounterText(container)).toBe('Counter: -2')
+})
+
+describe('should call saga: ', () => {
+  const sagaCallback = jest.fn()
 
   function* saga() {
     yield takeEvery(types.INCREASE, sagaCallback)
@@ -34,68 +61,30 @@ beforeEach(() => {
     yield takeEvery(types.RESET, sagaCallback)
   }
 
-  wrapper = mount(
-    <LocalReducer
-      reducer={reducer}
-      actions={actions}
-      saga={saga}
-      middleware={[middleware]}
-      children={({ counter }) => <h1>Counter: {counter}</h1>}
-    />,
+  const container = render(<LocalReducer {...sharedProps} saga={saga} />)
+
+  fireEvent.click(getIncreaseButton(container))
+  expect(sagaCallback.mock.calls).toHaveLength(1)
+  fireEvent.click(getDecreaseButton(container))
+  expect(sagaCallback.mock.calls).toHaveLength(2)
+  fireEvent.click(getResetButton(container))
+  expect(sagaCallback.mock.calls).toHaveLength(3)
+})
+
+test('should handle middleware ', () => {
+  const middlewareCallback = jest.fn()
+
+  const middleware = _store => next => action => {
+    middlewareCallback()
+    return next(action)
+  }
+
+  const container = render(
+    <LocalReducer {...sharedProps} middleware={[middleware]} />,
   )
-})
 
-describe('should render different states based on actions: ', () => {
-  /* -------------------> Increase <------------------- */
-  test('should handle increase correctly', () => {
-    expect(wrapper.instance().state).toMatchSnapshot()
-    wrapper.instance().dispatch(actions.increase())
-    expect(wrapper.instance().state).toMatchSnapshot()
-    wrapper.instance().boundActions.increase()
-    expect(wrapper.instance().state).toMatchSnapshot()
-  })
-
-  /* -------------------> Reset dispatch <------------------- */
-  test('should handle reset with dispatch correctly', () => {
-    wrapper.instance().dispatch(actions.reset())
-    expect(wrapper.instance().state).toMatchSnapshot()
-  })
-
-  /* -------------------> Decrease <------------------- */
-  test('should handle decrease correctly', () => {
-    wrapper.instance().dispatch(actions.decrease())
-    expect(wrapper.instance().state).toMatchSnapshot()
-    wrapper.instance().boundActions.decrease()
-    expect(wrapper.instance().state).toMatchSnapshot()
-  })
-
-  /* -------------------> Reset <------------------- */
-  test('should handle reset correctly', () => {
-    wrapper.instance().boundActions.reset()
-    expect(wrapper.instance().state).toMatchSnapshot()
-  })
-})
-
-describe('should call saga: ', () => {
-  test('should handle increase', () => {
-    wrapper.instance().boundActions.increase()
-    expect(sagaCallback.mock.calls).toHaveLength(1)
-  })
-
-  test('should handle decrease', () => {
-    wrapper.instance().boundActions.decrease()
-    expect(sagaCallback.mock.calls).toHaveLength(1)
-  })
-
-  test('should handle reset', () => {
-    wrapper.instance().boundActions.reset()
-    expect(sagaCallback.mock.calls).toHaveLength(1)
-  })
-})
-
-test('should handle middleware: ', () => {
-  wrapper.instance().dispatch({ type: 'TOTALY_FAKE_ACTION_1' })
+  fireEvent.click(getIncreaseButton(container))
   expect(middlewareCallback.mock.calls).toHaveLength(1)
-  wrapper.instance().dispatch({ type: 'TOTALY_FAKE_ACTION_2' })
+  fireEvent.click(getDecreaseButton(container))
   expect(middlewareCallback.mock.calls).toHaveLength(2)
 })
